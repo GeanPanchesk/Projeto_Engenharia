9059f6280335a9d6b3e17f277f43de182b13f20a
jlouren777 <jloucmello@protonmail.com>
2025-11-28 02:37:11 -0300

    Login arrumado fix #17

--- projeto_completo/backend/main/views.py
+++ projeto_completo/backend/main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 from .forms import CustomUserCreationForm
 
 
@@ -7,10 +8,19 @@ def index(request):
     return render(request, 'main/index.html')
 
 def index_login(request):
-    return render(request, 'main/index_login.html')
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
 
-def index_cadastro(request):
-    return render(request, 'main/index_cadastro.html')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('index')  # ou outra página depois do login
+        else:
+            messages.error(request, "Usuário ou senha incorretos!")
+
+    return render(request, 'main/index_login.html')
 
 def trailer(request):
     return render(request, 'main/trailer.html')
